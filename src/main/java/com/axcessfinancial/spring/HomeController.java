4bd42386534660a42bddc8a1f69a6bad8c045751
Carlos Jiga <c.jiga1983@gmail.com>
2012-11-29 18:08:08 -0500

    "Proyecto Spring-MVC"

---
+++ src/main/java/com/axcessfinancial/spring/HomeController.java
@@ -0,0 +1,43 @@
+package com.axcessfinancial.spring;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+
+/** @Controller:  Esto indica que es un controlador y cuando la aplicación escanee el paquete lo encontrara.*/
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	/**@RequestMapping: esta anotación captura la petición que es igual al valor de value y se realiza por el método Get, 
+	 * 	al ser la petición raíz se está indicando que esta será la página de inicio de la aplicación. */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+	<!-- <link rel="stylesheet" href="{${pageContext.request.contextPath}/resources/css/estilos.css" type="text/css">  -->
+	<title>Portal Docente Spring</title>
+</head>
+<body>
+ <div>	
+	<%@include file="head.jsp" %>
+	<%@include file="menu.jsp" %>
+	<%@include file="content.jsp" %>
+ </div>
+</body>
+</html>
